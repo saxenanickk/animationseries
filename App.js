b23263e63aded16005beb844c47d47fcf9a23ea8
Nikhil Saxena <saxenanickk@gmail.com>
2018-11-30 18:26:36 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import TwitterHeaderAnimation from "./TwitterHeaderAnimation";
+
+export default class App extends Component {
+	render() {
+		return <TwitterHeaderAnimation />;
+	}
+}
---
+++ TwitterHeaderAnimation.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import { Text, View, Image, ScrollView, Animated } from "react-native";
+
+HEADER_MAX_HEIGHT = 120;
+HEADER_MIN_HEIGHT = 70;
+PROFILE_IMAGE_MAX_HEIGHT = 80;
+PROFILE_IMAGE_MIN_HEIGHT = 40;
+export default class TwitterHeaderAnimation extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			scrollY: new Animated.Value(0)
+		};
+	}
+	render() {
+		const headerHeight = this.state.scrollY.interpolate({
+			inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+			outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+			extrapolate: "clamp"
+		});
+		const profileImageHeight = this.state.scrollY.interpolate({
+			inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+			outputRange: [PROFILE_IMAGE_MAX_HEIGHT, PROFILE_IMAGE_MIN_HEIGHT],
+			extrapolate: "clamp"
+		});
+		const profileImageMarginTop = this.state.scrollY.interpolate({
+			inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+			outputRange: [
+				HEADER_MAX_HEIGHT - PROFILE_IMAGE_MAX_HEIGHT / 2,
+				HEADER_MAX_HEIGHT + 5
+			],
+			extrapolate: "clamp"
+		});
+		const headerZindex = this.state.scrollY.interpolate({
+			inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+			outputRange: [0, 1],
+			extrapolate: "clamp"
+		});
+		const headerTitleBottom = this.state.scrollY.interpolate({
+			inputRange: [
+				0,
+				HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT,
+				HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT + 5 + PROFILE_IMAGE_MIN_HEIGHT,
+				HEADER_MAX_HEIGHT -
+					HEADER_MIN_HEIGHT +
+					5 +
+					PROFILE_IMAGE_MIN_HEIGHT +
+					26
+			],
+			outputRange: [-20, -20, -20, 0],
+			extrapolate: "clamp"
+		});
+		return (
+			<View style={{ flex: 1 }}>
+				<Animated.View
+					style={{
+						position: "absolute",
+						top: 0,
+						left: 0,
+						right: 0,
+						backgroundColor: "lightskyblue",
+						height: headerHeight,
+						zIndex: headerZindex,
+						alignItems: "center"
+					}}
+				>
+					<Animated.View
+						style={{ position: "absolute", bottom: headerTitleBottom }}
+					>
+						<Text
+							style={{ color: "#ffffff", fontSize: 14, fontWeight: "bold" }}
+						>
+							saxenanickk
+						</Text>
+					</Animated.View>
+				</Animated.View>
+				<ScrollView
+					style={{ flex: 1 }}
+					showsVerticalScrollIndicator={false}
+					scrollEventThrottle={16}
+					onScroll={Animated.event([
+						{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }
+					])}
+				>
+					<Animated.View
+						style={{
+							width: profileImageHeight,
+							height: profileImageHeight,
+							borderRadius: PROFILE_IMAGE_MAX_HEIGHT / 2,
+							borderColor: "white",
+							borderWidth: 3,
+							overflow: "hidden",
+							marginTop: profileImageMarginTop
+						}}
+					>
+						<Image
+							style={{ flex: 1, width: null, height: null }}
+							source={require("./me.jpg")}
+						/>
+					</Animated.View>
+					<View>
+						<Text style={{ fontWeight: "bold", fontSize: 26, paddingLeft: 10 }}>
+							saxenanickk
+						</Text>
+					</View>
+					<View style={{ height: 1000 }} />
+				</ScrollView>
+			</View>
+		);
+	}
+}
